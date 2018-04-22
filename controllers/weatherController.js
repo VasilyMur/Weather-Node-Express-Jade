e281d83d12486d6561e0c4ff4ca1b2f68913a023
Vasily Muravyev <vmouravi@gmail.com>
2018-04-22 14:07:37 +0300

    testing part 2 nodemailer

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -4,6 +4,9 @@ const util = require('util');
 const fs = require('fs');
 const appendFile = util.promisify(fs.appendFile);
 
+const nodemailer = require('nodemailer');
+const promisify = require('es6-promisify');
+
 
 // GET
 exports.forecast = (req, res) => {
@@ -57,18 +60,46 @@ exports.receive = (req, res) => {
     res.render('test');
 }
 
-exports.receivePost = async (req, res) => {
 
 
-    try {
 
-        const filePath = './public/uploads/data.txt';
-         const writeFile = await appendFile(filePath, JSON.stringify(req.body));
-         //res.render('fail', { title: 'Оплата не Прошла'});
-         res.status(200).send();
-      
-       } catch (e) {
-        res.render('error', {message:'Something went wrong'});
-       }
+exports.receivePost = (req, res) => {
+
+    const transport = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS
+        }
+      });
+    
+      const mailOptions = {
+            from: `YANDEX FUC....`,
+            to: '2011mckinsey@gmail.com',
+            subject: 'Website contact form',
+            text: 'This will be filled later',
+            html: `TEXT: ${JSON.stringify(req.body)}`
+    
+        };
+    
+
+        transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log("success")
+    
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
+    //   const sendMail = promisify(transport.sendMail, transport);
+    
+    //   sendMail(mailOptions).then((info) => {
+    //       console.log(info)
+        res.status(200).send();
+    //   }).catch((err) => {
+    //     res.render('error', {message:'Something went wrong'});
+    //   })
       
 }
